0b26b497e525bf6fc8745d694fff026f7df2ec90
DannyChiquillo <87905643+DannyChiquillo@users.noreply.github.com>
2021-09-11 13:04:17 -0600

    tercer commit

---
+++ programa.py
@@ -0,0 +1,10 @@
+usuario= str(input('Coloque su usuario'))
+contraseña = str(input('Coloque su contraseña'))
+
+x = "Danny"
+
+if usuario == "Danny" and contraseña == "0258":
+    print ("Inicio de sesion correcto")
+
+else: 
+    print ("Nombre de usuario incorrecto")
